b31cb3f2b156e44676ad0e4b2d5c6b5bf497eb5d
Vitali Lund <vitalilund@gmail.com>
2026-03-20 17:26:40 +0200

    feat: add cpp00 ex00 and piscine d00 namespace exercise

---
+++ CPP_00/ex00/megaphone.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cctype>
+
+int main(int argc, char **argv)
+{
+	if (argc != 1)
+	{
+		for(int i = 1; argv[i]; i++)
+		{
+			for(int j = 0; argv[i][j]; j++)
+				std::cout << static_cast<char>(std::toupper(argv[i][j]));
+		}
+		std::cout << "\n";
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	return (0);
+}
